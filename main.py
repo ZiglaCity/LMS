05f6632ebc9bbffe6f75981317cadeeff186f86b
ZiglaCity <ziglacity@gmail.com>
2024-11-25 23:04:19 +0000

    show a message when the user does not input enough details of the book

--- main.py
+++ main.py
@@ -159,6 +159,9 @@ def borrow_action(entries):
     # entries = {}
     if "Name" and "ID" and "Email" not in entries:
         messagebox.showinfo("Incorrect Details!", "Please input all borrower details to proceed")
+    if "Title" and "Genre" and "Author" not in entries:
+        messagebox.showinfo("Incorrect Details!", "Please input all book details to borrow")
+        
 
 def return_action(entries):
     data = {field: entry.get() for field, entry in entries.items()}
