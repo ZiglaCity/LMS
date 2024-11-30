fa1636e3eda4353240a149921caa80550a5a5b47
ZiglaCity <ziglacity@gmail.com>
2024-11-30 05:07:52 +0000

    Added style to search phase

--- main.py
+++ main.py
@@ -593,6 +593,44 @@ def search_phase():
         books = cursor.fetchall()
         open_search_result(books)
 
+    
+    style = ttk.Style()
+
+    style.configure(
+        "Modern.TLabelframe",
+        font=("Helvetica", 14, "bold"), 
+        background="#f9f9f9",
+        foreground="#333", 
+        relief="ridge",
+        padding=10 
+    )
+    style.configure(
+        "Modern.TLabelframe.Label",
+        background="#f9f9f9",
+        foreground="#333",
+        font=("Helvetica", 16, "bold")
+    )
+
+    style.configure(
+        "TEntry",
+        font=("Helvetica", 12),  
+        padding=5
+    )
+
+    style.configure(
+        "TButton",
+        font=("Helvetica", 12, "bold"),
+        background="#4CAF50",
+        foreground="white",
+        padding=10
+    )
+    style.map(
+        "TButton",
+        background=[("active", "#45a049")],
+        relief=[("pressed", "sunken"), ("!pressed", "raised")]
+    )
+
+
 
 def borrow_phase():
     root.title("Zigla's LMS - Borrow Books")
